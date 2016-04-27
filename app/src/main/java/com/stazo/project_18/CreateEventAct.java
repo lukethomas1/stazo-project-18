3353db784f191a0bbcf0f0c2602f75632c1e1997
Brian Chan <alto1020@gmail.com>
2016-04-27 01:33:50 -0700

    Edited CreateEvent screen

--- app/src/main/java/com/stazo/project_18/CreateEventAct.java
+++ app/src/main/java/com/stazo/project_18/CreateEventAct.java
@@ -2,8 +2,13 @@ package com.stazo.project_18;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class CreateEvent extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreateEventAct extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
