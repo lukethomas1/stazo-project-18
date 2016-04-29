73b42a80906ee41364ceab41b34831c94f19e2a1
Gates Zeng <gatesazeng@gmail.com>
2016-04-28 20:23:30 -0700

    Working on Login

--- app/src/main/java/com/stazo/project_18/FBKeyHash.java
+++ app/src/main/java/com/stazo/project_18/FBKeyHash.java
@@ -1,7 +1,38 @@
 package com.stazo.project_18;
 
+import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * Created by Gates on 4/28/2016.
  */
-public class FBKeyHash {
+public class FBKeyHash extends Application {
+    public void onCreate() {
+        super.onCreate();
+        printHashKey();
+    }
+
+    public void printHashKey() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.stazo.project_18",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+    }
 }
