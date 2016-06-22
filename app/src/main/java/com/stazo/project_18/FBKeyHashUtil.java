4d527207ecb033612eac2c94210cac979f41d66a
Gates Zeng <Gates Zeng>
2016-06-21 19:05:47 -0700

    new keyhash program

--- app/src/main/java/com/stazo/project_18/FBKeyHashUtil.java
+++ app/src/main/java/com/stazo/project_18/FBKeyHashUtil.java
@@ -1,21 +1,22 @@
 package com.stazo.project_18;
 
-import android.app.Application;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-/**
- * Created by Gates on 4/28/2016.
- */
-public class FBKeyHash extends Application {
-    public void onCreate() {
-        super.onCreate();
+public class FBKeyHashUtil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fbkey_hash_util);
         printHashKey();
     }
 
@@ -27,7 +28,7 @@ public class FBKeyHash extends Application {
             for (Signature signature : info.signatures) {
                 MessageDigest md = MessageDigest.getInstance("SHA");
                 md.update(signature.toByteArray());
-                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+                Log.e("FB KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
             }
         } catch (PackageManager.NameNotFoundException e) {
 
