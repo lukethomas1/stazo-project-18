84af5b03fa2d5c1ed645d1c9653a7e09985b0b2f
Isaac Wang <isaacwang4076@gmail.com>
2016-07-03 13:17:19 -0700

    JOINEDEVENT small bugfix

--- app/src/main/java/com/stazo/project_18/User.java
+++ app/src/main/java/com/stazo/project_18/User.java
@@ -368,7 +368,7 @@ public class User {
         creatorList.add(creator_id);
 
         (new NotificationJoinedEvent(Notification2.TYPE_JOINED_EVENT,
-                name, ID, event_name)).
+                name, event_id, event_name)).
                 pushToFirebase(fb, creatorList);
 
         // increment popularity
