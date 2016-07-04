14bba252c3b2ea6bb067b0c0f74a16d7f67ac0c0
Isaac Wang <isaacwang4076@gmail.com>
2016-07-03 17:29:00 -0700

    lmao

--- app/src/main/java/com/stazo/project_18/NotificationFrag.java
+++ app/src/main/java/com/stazo/project_18/NotificationFrag.java
@@ -206,7 +206,7 @@ public class NotificationFrag extends android.support.v4.app.Fragment {
 
                 // There are no notifications to show
                 if(notifs.isEmpty()) {
-                    ((TextView)v.findViewById(R.id.noNotificationsText)).setText("No Notifications");
+                    ((TextView)v.findViewById(R.id.noNotificationsText)).setText("You have no notifications");
                 }
 
                 // There are notifications to show, show them
